bd5c0c75d2813b36ee6f2ff04a38a7ad1150d2f0
Eleven-junichi2 <junichinomori@yahoo.co.jp>
2018-10-08 15:14:45 +0900

    fix .gitignore

---
+++ mamo/main.py
@@ -0,0 +1,11 @@
+from kivy.app import App
+from kivy.uix.widget import Widget
+
+
+class MamoApp(App):
+    def build(self):
+        return Widget()
+
+
+if __name__ == '__main__':
+    MamoApp().run()
