573d67d7fa5d99026247d2a48257b2257f822484
Eleven-junichi2 <junichinomori@yahoo.co.jp>
2018-10-13 16:11:04 +0900

    Fix button designs and add memo management menu.

--- mamo/main.py
+++ mamo/main.py
@@ -1,11 +1,41 @@
 from kivy.app import App
-from kivy.uix.widget import Widget
+from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.core.window import Window
+from kivy.core.text import LabelBase
+
+LabelBase.register("NotoSansCJKjp-Light",
+                   fn_regular="./fonts/NotoSansCJKjp-Light.otf")
+
+Window.size = (360, 640)
+
+
+class Tutorial(Screen):
+    pass
+
+
+class ManageMemoMenu(Screen):
+    pass
+
+
+class MainMenu(Screen):
+    pass
+
+
+class MamoScreenManager(ScreenManager):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.add_widget(MainMenu(name="main"))
+        self.add_widget(ManageMemoMenu(name="manage_memo"))
+        self.add_widget(Tutorial(name="tutorial"))
 
 
 class MamoApp(App):
     def build(self):
-        return Widget()
+        self.title = "Mamo"
+        self.icon = "images/mamo.png"
+        return MamoScreenManager()
 
 
 if __name__ == '__main__':
+    print(LabelBase.get_system_fonts_dir())
     MamoApp().run()
