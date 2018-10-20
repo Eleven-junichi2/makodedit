679a9028aea26acd4cf345a700e779713ebfb40a
Eleven-junichi2 <junichinomori@yahoo.co.jp>
2018-10-20 12:26:47 +0900

    Remove cache

--- deploy-with-pyinstaller.sh
+++
@@ -1,4 +0,0 @@
-LD_LIBRARY_PATH=~/.pyenv/versions/3.7.0/lib python -m PyInstaller --name makodedit makodedit/main.py -F
-cp -v "makodedit/makodedit.kv" "dist/"
-cp -v -r "makodedit/images/" "dist/images/"
-cp -v -r "makodedit/fonts/" "dist/fonts/"
