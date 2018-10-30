8cf32c226a91f29b2294e46e08f39d7843fbb592
Eleven-junichi2 <junichinomori@yahoo.co.jp>
2018-10-30 17:55:10 +0900

    Update requests package

--- deploy-with-pyinstaller.sh
+++ deploy-with-pyinstaller.sh
@@ -1,3 +1,4 @@
+rm -r "dist/"
 LD_LIBRARY_PATH=~/.pyenv/versions/3.7.0/lib python -m PyInstaller --name makodedit -F makodedit/main.py
 cp -v "makodedit/makodedit.kv" "dist/"
 cp -v -r "makodedit/images/" "dist/images/"
